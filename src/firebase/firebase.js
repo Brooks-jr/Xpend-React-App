ac267d956335389fc8456e91e0e8cd976db9a3d6
Brooks Von Doom <jeffery.brooks@loop.colum.edu>
2018-08-26 15:02:16 -0500

    jest tests & firebase database configs + promises

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,25 +13,159 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().set({
-    name: 'Bruce Wayne',
-    alterEgo: 'The Batman',
-    hasPowers: false,
-    location: {
-        city: 'Gotham',
-        country: 'United States'
-    }
-});
+export { firebase, database as default };
 
-// database.ref().set('this is my data!');
 
-database.ref('name').set('Unknown');
+
+
+
+
+
+// // listen for when a child is removed
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+
+// // listen for when a child is changed
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+
+// // listen for when a child is added
+// database.ref('expenses').on('child_added', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+
+// database.ref('expenses')
+// .once('value')
+// .then((snapshot) => {
+//     const expenses = [];
+
+//     snapshot.forEach((childSnapshot) => {
+//         expenses.push({
+//             id: childSnapshot.key,
+//             ...childSnapshot.val()
+//         });
+//     });
+
+//     console.log(expenses);
+// });
+
+// database.ref('expenses').on('value', (snapshot) => {
+//     const expenses = [];
+
+//     snapshot.forEach((childSnapshot) => {
+//         expenses.push({
+//             id: childSnapshot.key,
+//             ...childSnapshot.val()
+//         });
+//     });
+
+//     console.log(expenses);
+// });
+
+// database.ref('expenses').push({
+//     description: 'gas bill',
+//     note: '',
+//     amount: 30000,
+//     createdAt: 2114415 
+// });
+
+// database.ref('notes/-LKd-5sonm47Uytvf9Uv').remove();
+
+// database.ref('notes').push({
+//     title: 'firebase lesson',
+//     body: 'handling list based data'
+// })
+
+// database.ref().on('value', (snapshot) => {
+//     const val = snapshot.val();
+//     console.log(`${val.alterEgo} is a vigilante in ${val.location.city} that wears ${val.appearence.costume}`)
+// });
+
+// ================
+//      READ
+// ================
+// const onValueChange = database.ref().on('value', (snapshot) => {
+//     console.log(snapshot.val());
+// }, (error) => {
+//     console.log('Error: our engines are failing!', error);
+// });
+
+// setTimeout(() => {
+//     database.ref('appearence/weight').set('197lbs');
+// }, 3500);
+
+// setTimeout(() => {
+//     database.ref().off(onValueChange);
+// }, 7000);
+
+// setTimeout(() => {
+//     database.ref('appearence/weight').set('200lbs');
+// }, 10500);
+
+// database.ref()
+// .once('value')
+// .then((snapshot) => {
+//     const val = snapshot.val();
+//     console.log(val);
+// })
+// .catch((error) => {
+//     console.log('Error: our engines are failing!', error);
+// });
+
+// ================
+//      CREATE
+// ================
+// database.ref().set({
+//     name: 'Bruce Wayne',
+//     alterEgo: 'The Batman',
+//     hasPowers: false,
+//     killsPeople: false,
+//     location: {
+//         city: 'Gotham',
+//         country: 'United States'
+//     }
+// }).then(() => {
+//     console.log('data has been stored');
+// }).catch((e) => {
+//     console.log('our engines are failing!', e);
+// });
+
+// database.ref().set('dummy data here!');
+
+// database.ref('name').set('Unknown');
 // database.ref('location/city').set('Metropolis')
 
-database.ref('appearence').set({
-    height: '6ft 2in',
-    weight: '210lbs',
-    costume: 'Black and grey suit with a bat symbol on the chest and a cape. Cowl with pointed ears on top.' 
-});
+// database.ref('appearence').set({
+//     height: '6ft 2in',
+//     weight: '210lbs',
+//     costume: 'Black and grey suit with a bat symbol on the chest and a cape. Cowl with pointed ears on top.'
+// }).then(() => {
+//     console.log('second set of data stored');
+// }).catch((e) => {
+//     console.log('our engines are failing!', e);
+// });
 
-console.log('requested data change')
+// ================
+//     UPDATE
+// ================
+// database.ref().update({
+//     name: 'Tim Drake',
+//     alterEgo: 'Future Batman',
+//     killsPeople: true,
+//     'location/city': 'bludhaven'
+// });
+
+// ================
+//     DELETE
+// ================
+// database.ref('name').set(null);
+// database.ref('name').remove()
+//     .then(() => {
+//         console.log('data has been removed');
+//     }).catch((e) => {
+//         console.log('our engines are failing!', e);
+//     });
+
+// console.log('requested data change!')
--- src/playground/promises.js
+++ src/playground/promises.js
@@ -2,11 +2,11 @@
 
 const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-        // resolve({
-        //     name: 'Bruce',
-        //     alterego: 'Batman'
-        // });
-        reject('oops, something went wrong, dude');
+        resolve({
+            name: 'Bruce',
+            alterego: 'Batman'
+        });
+        // reject('oops, something went wrong, my dude');
     }, 5000);
 });
 
@@ -14,6 +14,14 @@ console.log('before');
 
 promise.then((data) => {
     console.log('1', data);
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('This is another promise');
+        }, 5000);
+    });
+}).then((str) => {
+    console.log('does this run?', str)
 }).catch((error) => {
     console.log('error:', error);
 });
