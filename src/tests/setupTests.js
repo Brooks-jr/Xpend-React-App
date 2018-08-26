d80365acb0b1ff75d9351d64608e01774eff8b82
Brooks Von Doom <jeffery.brooks@loop.colum.edu>
2018-08-26 16:50:43 -0500

    config firebase databases for testing & development

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,9 +1,13 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import DotEnv from 'dotenv';
 
 Enzyme.configure({
     adapter: new Adapter()
 });
+
+DotEnv.config({ path: '.env.test' });
+
 
 // import { configure } from 'enzyme';
 // import Adapter from 'enzyme-adapter-react-16';
