bfdd8de1ad2a4521760c2f68a35d68750ec72043
Brooks Von Doom <jeffery.brooks@loop.colum.edu>
2018-08-27 15:43:10 -0500

    Firebase Login and Logout w/ Google Authentication

--- src/routers/app-router.js
+++ src/routers/app-router.js
@@ -6,13 +6,15 @@ import AddPage from '../components/add-expense';
 import EditPage from '../components/edit-expense';
 import HelpPage from '../components/help';
 import NotFoundPage from '../components/404';
+import LoginPage from '../components/login';
 
 const AppRouter = () => (
     <BrowserRouter>
         <div>
             <Header />
             <Switch>
-                <Route path="/" component={DashboardPage} exact={true}/>
+                <Route path="/" component={LoginPage} exact={true} />
+                <Route path="/dashboard" component={DashboardPage} />
                 <Route path="/create" component={AddPage} />
                 <Route path="/edit/:id" component={EditPage} />
                 <Route path="/help" component={HelpPage} />
