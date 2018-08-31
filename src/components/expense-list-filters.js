0738ec0d3b11ae100071b501a77e9190242aff34
Brooks Von Doom <jeffery.brooks@loop.colum.edu>
2018-08-30 19:49:42 -0500

    some styling

--- src/components/expense-list-filters.js
+++ src/components/expense-list-filters.js
@@ -26,33 +26,50 @@ export class ExpenseListFilters extends React.Component {
     }
     render() {
         return (
-            <div>
-                <input
-                    type="text"
-                    value={this.props.filters.text}
-                    onChange={this.onTextChange}
-                />
+            <div className="container">
+                <div className="input-group">
+                    <div className="input-group__item">
+                        <input
+                            className="text-input"
+                            type="text"
+                            placeholder="Search Expenses..."
+                            value={this.props.filters.text}
+                            onChange={this.onTextChange}
+                        />
+                    </div>
 
-                <select
-                    value={this.props.filters.sortBy}
-                    onChange={this.onSortChange}
-                >
-                    <option value="date">Date</option>
-                    <option value="amount">Amount</option>
-                </select>
+                    <div className="input-group__item">
+                        <select
+                            className="select"
+                            value={this.props.filters.sortBy}
+                            onChange={this.onSortChange}
+                        >
+                            <option value="date">Date</option>
+                            <option value="amount">Amount</option>
+                        </select>
+                    </div>
 
-                <DateRangePicker
-                    startDate={this.props.filters.startDate}
-                    startDateId="your_unique_start_date_id"
-                    endDate={this.props.filters.endDate}
-                    endDateId="your_unique_end_date_id"
-                    onDatesChange={this.onDatesChange}
-                    focusedInput={this.state.calendarFocused}
-                    onFocusChange={this.onFocusChange}
-                    showClearDates={true}
-                    numberOfMonths={1}
-                    isOutsideRange={() => false}
-                />
+                    <div className="input-group__item">
+                        <DateRangePicker
+                            startDate={this.props.filters.startDate}
+                            startDateId="your_unique_start_date_id"
+                            endDate={this.props.filters.endDate}
+                            endDateId="your_unique_end_date_id"
+                            onDatesChange={this.onDatesChange}
+                            focusedInput={this.state.calendarFocused}
+                            onFocusChange={this.onFocusChange}
+                            showClearDates={true}
+                            numberOfMonths={1}
+                            isOutsideRange={() => false}
+                        />
+                    </div>
+                </div>
+
+
+
+
+
+
             </div>
         )
     }
--- src/components/login.js
+++ src/components/login.js
@@ -7,7 +7,7 @@ export const LoginPage = ({ startLogin }) => (
         <div className="box-layout__inner-box">
             <h1 className="inner-box__header"><span>X</span>pend</h1>
             <p className="inner-box__text">Start managing your expenses by signing in!</p>
-            <button className="button inner-box__button" onClick={startLogin}>Sign In With <span>Google</span></button>
+            <button className="button button__hover inner-box__button" onClick={startLogin}>Sign In With <span>Google</span></button>
             <p className="inner-box__github">Xpend @ github.com/Brooks-jr/Xpend-React-App</p>
         </div>
     </div>
